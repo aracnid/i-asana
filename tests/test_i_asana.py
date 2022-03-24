4c2a458918c36efb64c5a56fc0a3529668b866b3
Jason Romano <aracnid@gmail.com>
2022-03-24 08:36:09 -0400

    Separate demo tests

--- tests/test_i_asana.py
+++ tests/test_i_asana.py
@@ -26,19 +26,3 @@ def test_init_asana(asana):
     me = asana.client.users.me()
 
     assert me['workspaces'][0]['name'] == 'lakeannebrewhouse.com'
-
-def test_get_project_by_id(asana):
-    """Tests project get functionality.
-    """
-    result = asana.client.projects.get_project(project_id)
-
-    assert result['gid'] == project_id
-    assert result['name'] == 'BrewOps'
-
-def test_get_tasks_for_project(asana):
-    """Tests get_tasks_for_project functionality.
-    """
-    tasks = asana.client.tasks.get_tasks_for_project(project_id)
-    for task in tasks:
-        assert task
-        logger.info(task['name'])
