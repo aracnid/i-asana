e59763dbf011eea6c31602af8655f100c54891d8
Jason Romano <aracnid@gmail.com>
2022-11-02 08:24:39 -0400

    Update package versions

--- tests/test_init.py
+++ tests/test_init.py
@@ -1,10 +1,9 @@
 """Test functions for i-Asana import.
 """
-from i_asana import __version__
+import i_asana
 
 def test_version():
     """Tests that i-Asana was imported successfully.
     """
-    assert __version__
-    assert __version__ == '1.0.0-alpha.5'
+    assert i_asana.__version__
     
